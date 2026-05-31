7f8425ee7f2af4e49ed6e3647c1e3d70cf710f59
WalletBeds <thewalletbeds@gmail.com>
2026-05-31 14:20:43 -0700

    Fix root index and images for Cloudflare Pages

---
+++ replace_images.py
@@ -0,0 +1,19 @@
+import os
+
+replacements = {
+    "images/upholstered_black.jpg": "images/WalletBedHeroImage.png",
+    "images/brand_lv.jpg": "images/WalletBedMidnightTuftedEdition.png",
+    "images/brand_goyard.jpg": "images/WalletBedRoyalBlueTufted.png",
+    "images/brand_valentino.jpg": "images/WalletBedIndustrialPipeEdition.png",
+    "images/brand_swiss.jpg": "images/WalletBedClassicSleighEdition.png",
+    "images/brand_gucci.jpg": "images/WalletBedFourPosterCanopy.png",
+    "images/brand_white.jpg": "images/WalletBedMinimalistPlatform.png"
+}
+
+for file in ["walletbeds/index.html", "walletbeds/script.js"]:
+    with open(file, "r", encoding="utf-8") as f:
+        content = f.read()
+    for old, new in replacements.items():
+        content = content.replace(old, new)
+    with open(file, "w", encoding="utf-8") as f:
+        f.write(content)
